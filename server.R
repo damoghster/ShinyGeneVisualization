b4b5d746b12fb033f5ebeeccddf83e6e516e87cc
Amogh Mantri <amogh.mantri@yahoo.com>
2020-08-22 22:46:28 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,7 +1,9 @@
 #source("global.R")
 library(dplyr)
-options(shiny.maxRequestSize=1000*1024^2)
-server<-shinyServer(function(input, output,session) {
+library(tidyr)
+library(ggplot2)
+options(shiny.maxRequestSize = 1000 * 1024 ^ 2)
+server <- shinyServer(function(input, output, session) {
   # to be modified to incorporate multiple datasets later
   #   Return the requested dataset
   # datasetInput <- AT2_dat#reactive({
@@ -17,126 +19,190 @@ server<-shinyServer(function(input, output,session) {
   #})
   
   
-  selNum<- reactive({
-    out<- switch(input$colSel,
+  selNum <- reactive({
+    out <- switch(input$colSel,
                   one = 1, two = 2)
     return(out)
   })
+ 
   
-  at2 <- reactive({
-    req(input$file) 
-    req(input$gFile)
-    out <-readr::read_csv(input$file$datapath)
-    return(out)
-  })
   groups <- reactive({
     req(input$gFile)
-    out <-readr::read_csv(input$gFile$datapath)
+    out <- readr::read_csv(input$gFile$datapath)
     return(out)
   })
   
   all_ids <- reactive({
-    req(input$file) 
+    req(input$file)
     req(input$gFile)
-    at2_in <- at2()
+    primary <- primary()
     selectionNum <- selNum()
-    out<-unique(at2_in[,selectionNum]) #change 1 to variable that can be selected by user
+    out <-
+      unique(primary[, selectionNum]) #change 1 to variable that can be selected by user
     return(out)
   })
   
   choiceList <- reactive({
-    req(input$file) 
+    req(input$file)
     req(input$gFile)
     return(c(all_ids()))
   })
   
   
-  df <- reactive({
-    req(input$file) 
-    req(input$gFile)
-    at2_in <- at2()
-    selectionNum <- selNum()
-     out <- at2_in %>% filter(at2_in[,selectionNum]==as.character(input$mygene)) %>% distinct(colnames(at2_in, do.NULL = TRUE, prefix = "col")[1], .keep_all = TRUE) %>% select(3:ncol(at2_in))
-     return(out)
+  
+  
+  primary <- reactive({
+   
+    infile <- input$data
+    if (is.null(infile)){
+      return(NULL)
+    }
+    out <- readr::read_csv(paste0('./Primary/',infile))
+    return(out)
   })
-  xData <- reactive({
-    req(input$file) 
-    req(input$gFile)
-    dfIn <- df()
+  
+  dfPri <- reactive({
+   
+    primary <- primary()
+    selectionNum <- selNum()
+    out <-
+      primary %>% filter(primary[, selectionNum] == as.character(input$mygene)) %>% distinct(colnames(primary, do.NULL = TRUE, prefix = "col")[1], .keep_all = TRUE) %>% select(3:ncol(primary))
+    return(out)
+  })
+  
+  xDataPri <- reactive({
+    
+    dfIn <- dfPri()
     out <- names(dfIn)
-    print(out)
+    
     return(out)
   })
-  yData <- reactive({
-    req(input$file) 
-    req(input$gFile)
-    dfIn <- df()
+  
+  yDataPri <- reactive({
+   
+    dfIn <- dfPri()
     out <- as.numeric(dfIn)
-    print(out)
     return(out)
   })
   
-  output$view<-DT::renderDataTable({
-    req(input$file) 
+  
+  
+  at2 <- reactive({
+    req(input$file)
     req(input$gFile)
-    at2_in <- at2()
-    DT::datatable(at2_in %>% filter(at2_in[, 1]==as.character(input$mygene))) #make dynamic later
+    out <- readr::read_csv(input$file$datapath)
+    return(out)
   })
   
-  output$view2<-DT::renderDataTable({
-    req(input$file) 
+  
+  dfSec <- reactive({
+    req(input$file)
     req(input$gFile)
     at2_in <- at2()
     selectionNum <- selNum()
-    DT::datatable(at2_in %>% filter(at2_in[,selectionNum]==as.character(input$mygene)) %>% distinct(colnames(at2_in, do.NULL = TRUE, prefix = "col")[1], .keep_all = TRUE) %>% select(3:ncol(at2_in)))
+    out <-
+      at2_in %>% filter(at2_in[, selectionNum] == as.character(input$mygene)) %>% distinct(colnames(at2_in, do.NULL = TRUE, prefix = "col")[1], .keep_all = TRUE) %>% select(3:ncol(at2_in))
+    return(out)
   })
+  
+  xDataSec <- reactive({
+    req(input$file)
+    req(input$gFile)
+    dfIn <- dfSec()
+    out <- names(dfIn)
+    
+    return(out)
+  })
+  yDataSec <- reactive({
+    req(input$file)
+    req(input$gFile)
+    dfIn <- dfSec()
+    out <- as.numeric(dfIn)
+    
+    return(out)
+  })
+  
+  output$view <- DT::renderDataTable({
+    req(input$file)
+    req(input$gFile)
+    at2_in <- at2()
+    DT::datatable(at2_in %>% filter(at2_in[, 1] == as.character(input$mygene))) #make dynamic later
+  })
+  
   
   #input$mygene
-  output$summary<- renderText({
-    "This application allows users to select a single gene of interest and 
+  output$summary <- renderText({
+    "This application allows users to select a single gene of interest and
     plot a interactive boxplot for gene expression data from AT-II cells"
   })
   
   output$select = renderUI({
-    req(input$file) 
+    req(input$file)
     req(input$gFile)
-    selectizeInput("mygene",
-                   label = h5("Enter your gene here : "),
-                   multiple = FALSE,
-                   selected = NULL,
-                   choices =  choiceList())
+    selectizeInput(
+      "mygene",
+      label = h5("Enter your gene here : "),
+      multiple = FALSE,
+      selected = NULL,
+      choices =  choiceList()
+    )
+  })
+  
+  output$primaryData = renderUI({
+    print(getwd())
+    print(ls())
+    selectInput(inputId = 'data',
+                label = 'Choose primary data',
+                choices = list.files(path = "./Primary",
+                                     full.names = FALSE,
+                                     recursive = FALSE))
   })
   
   
-  # output$uigene = renderPlotly({
-  #   # inputgene = input$mygene # updateSelectizeInput(session, 'mygene', choices  = c(unique(at2$Gene_Symbol)),
-  #     #                 server   = TRUE,
-  #            #          selected = "Ddx58")
-  #   req(input$file) req(input$gFile)
-  #    at2_in <- at2()
-  #   df<-at2_in %>% filter(Gene_Symbol==as.character(input$mygene))
-  #   #t<- #df %>% summarise(max(count)) %>% pull() %>% as.data.frame()
-  #   #t<-as.numeric(max(df$count))
-  #   set_y_axis <- list(title = "Counts (CPM)", range = c(0,(max(df$count)+0.15*max(df$count))))
-  #   set_x_axis_wt <- list(title = "Juvenile AT2")
-  #   
-  #   # font style # %>% layout(boxmode="group")
-  #   p1<-plot_ly(data=df %>% filter(genotype=="WT" & age=="Juvenile"), y = ~count, color = ~group,colors = juvenile_colors,
-  #               type = "box",width = 900, height = 800,jitter = 0.3, pointpos = 0, boxpoints = 'all') %>%
-  #     layout(xaxis=set_x_axis_wt,yaxis = set_y_axis)# %>% layout(annotations = juv_title)
-  #   p <- subplot(p1, titleX = TRUE, titleY = TRUE,nrows = 2,margin = 0.07) %>% layout(showlegend = TRUE)
-  #   p %>%
-  #   config(plot_ly(),
-  #          toImageButtonOptions= list(filename = as.character(input$mygene))) #,#width = 1000, #height =  350 # thi can be added to toImageButtonOptions
-  # })
+ 
   
-  output$uigene = renderPlotly({
-    req(input$file) 
+  
+  output$plotPrimary = renderPlotly({
+    xDataIn <- xDataPri()
+    yDataIn <- yDataPri()
+    p <-
+      plot_ly(
+        x = xDataIn,
+        y = yDataIn,
+        name = "Gene Counts",
+        type = "scatter"
+      )
+  })
+  
+  output$plotSecondary = renderPlotly({
+    req(input$file)
     req(input$gFile)
-    xDataIn <- xData()
+    xDataIn <- xDataSec()
     yDataIn <- yData()
-    p<-plot_ly(x=xDataIn, y=yDataIn,name = "Gene Counts",type = "scatter")
+    p <-
+      plot_ly(
+        x = xDataIn,
+        y = yDataIn,
+        name = "Gene Counts",
+        type = "scatter"
+      )
   })
+  
+  # output$plot2 <- renderPlot({
+  #   req(input$file)
+  #   req(input$gFile)
+  #   df_in <- dfSec()
+  #   group_in <- groups()
+  #   df_in %>%
+  #     gather(Sample, Value) %>%
+  #     mutate(Samples = gsub("\\d+", "", Sample)) %>%
+  #     left_join(group_in) %>%
+  #     mutate(Group = factor(Group)) %>%
+  #     ggplot(aes(Group, Value)) +
+  #     geom_boxplot() +
+  #     geom_jitter(aes(color  = Samples)) +
+  #     theme_bw()
+  # })
   #
   # p<- reactive({
   #  out<- plot_ly(x=xData, y=yData,name = "Gene Counts", type = "bar")
