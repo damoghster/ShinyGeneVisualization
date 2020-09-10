6f8c0983a51c708a77ab76fbb3d48a93daa717ce
Amogh Mantri <amogh.mantri@yahoo.com>
2020-09-10 18:51:12 -0500

    Grouping working

--- server.R
+++ server.R
@@ -1,6 +1,8 @@
 #source("global.R")
 library(dplyr)
 library(tidyr)
+library(ggplot2)
+library(plotly)
 
 options(shiny.maxRequestSize=1000*1024^2)
 server<-shinyServer(function(input, output,session) {
@@ -61,20 +63,41 @@ server<-shinyServer(function(input, output,session) {
      out <- at2_in %>% filter(at2_in[,selectionNum]==as.character(input$mygene)) %>% distinct(colnames(at2_in, do.NULL = TRUE, prefix = "col")[1], .keep_all = TRUE) %>% select(3:ncol(at2_in))
      return(out)
   })
-  xData <- reactive({
-    req(input$file) 
+  
+  # xData <- reactive({
+  #   req(input$file) 
+  #   req(input$gFile)
+  #   dfIn <- df()
+  #   out <- names(dfIn)
+  #   
+  #   return(out)
+  # })
+  # 
+  # yData <- reactive({
+  #   req(input$file) 
+  #   req(input$gFile)
+  #   dfIn <- df()
+  #   out <- as.numeric(dfIn)
+  # 
+  #   return(out)
+  # })
+  # 
+  formattedData <- reactive({
+    req(input$file)
     req(input$gFile)
-    dfIn <- df()
-    out <- names(dfIn)
-    print(out)
-    return(out)
-  })
-  yData <- reactive({
-    req(input$file) 
-    req(input$gFile)
-    dfIn <- df()
-    out <- as.numeric(dfIn)
-    print(out)
+    df_in <- df()
+    group_in <- groups()
+    df_in <- as.data.frame(t(df_in)) 
+    colnames(df_in) <- c("Count")
+    group_in <- as.data.frame(t(group_in))
+    colnames(group_in) <- c("Group")
+    group_in$Sample = row.names(group_in)
+    print(df_in)
+    print(group_in)
+    df_in <- cbind(df_in,group_in)
+    # df_in %>% group_by(Sample)
+    print(df_in)
+    out <-df_in
     return(out)
   })
   
@@ -90,7 +113,8 @@ server<-shinyServer(function(input, output,session) {
     req(input$gFile)
     at2_in <- at2()
     selectionNum <- selNum()
-    DT::datatable(at2_in %>% filter(at2_in[,selectionNum]==as.character(input$mygene)) %>% distinct(colnames(at2_in, do.NULL = TRUE, prefix = "col")[1], .keep_all = TRUE) %>% select(3:ncol(at2_in)))
+    f <- formattedData()
+    DT::datatable(f)
   })
   
   #input$mygene
@@ -123,43 +147,37 @@ server<-shinyServer(function(input, output,session) {
   #   set_x_axis_wt <- list(title = "Juvenile AT2")
   #   
   #   # font style # %>% layout(boxmode="group")
-  #   p1<-plot_ly(data=df %>% filter(genotype=="WT" & age=="Juvenile"), y = ~count, color = ~group,colors = juvenile_colors,
-  #               type = "box",width = 900, height = 800,jitter = 0.3, pointpos = 0, boxpoints = 'all') %>%
-  #     layout(xaxis=set_x_axis_wt,yaxis = set_y_axis)# %>% layout(annotations = juv_title)
-  #   p <- subplot(p1, titleX = TRUE, titleY = TRUE,nrows = 2,margin = 0.07) %>% layout(showlegend = TRUE)
+    # p1<-plot_ly(data=df_in,y = ~count, color = ~group,colors = juvenile_colors,
+    #             type = "box",width = 900, height = 800,jitter = 0.3, pointpos = 0, boxpoints = 'all') %>%
+    #   layout(xaxis=set_x_axis_wt,yaxis = set_y_axis)# %>% layout(annotations = juv_title)
+    # p <- subplot(p1, titleX = TRUE, titleY = TRUE,nrows = 2,margin = 0.07) %>% layout(showlegend = TRUE)
   #   p %>%
   #   config(plot_ly(),
   #          toImageButtonOptions= list(filename = as.character(input$mygene))) #,#width = 1000, #height =  350 # thi can be added to toImageButtonOptions
   # })
   
   output$uigene = renderPlotly({
-    req(input$file) 
-    req(input$gFile)
-    xDataIn <- xData()
-    yDataIn <- yData()
-    p<-plot_ly(x=xDataIn, y=yDataIn,name = "Gene Counts",type = "scatter")
-  })
-  
-  
-  output$plot2 <- renderPlot({
     req(input$file)
     req(input$gFile)
-    df_in <- df()
-    group_in <- groups()
-    print(group_in)
-    print( df_in %>%
-             gather(Sample, Value) %>%
-             mutate(Samples = gsub("\\d+", "", Sample)) %>%
-             left_join(group_in) %>%
-             mutate(Group = factor(Group)))
-    df_in %>%
-      gather(Sample, Value) %>%
-      mutate(Samples = gsub("\\d+", "", Sample)) %>%
-      left_join(group_in) %>%
-      mutate(Group = factor(Group)) %>%
-      ggplot(aes(Group, Value)) +
-      geom_boxplot() +
-      geom_jitter(aes(color  = Samples)) +
-      theme_bw()
+   data <- formattedData()
+   data$Group <- as.factor(data$Group)
+   p <- plot_ly(data = data, x = ~Group, y = ~Count, color = ~Group, type = "box",boxpoints = "all", jitter = .3,
+                pointpos = 0) %>% layout(boxmode = "group")
   })
+
+
+  # output$plot2 <- renderPlot({
+  #   req(input$file)
+  #   req(input$gFile)
+  #   df_in <- df()
+  #   # print(head(df_in %>%
+  #   #          gather(Sample, Value) %>%
+  #   #          mutate(Samples = gsub("\\d+", "", Sample)) %>%
+  #   #          mutate(Group = factor(Group))))
+  #   df_in %>%
+  #     ggplot(aes(Group, Count)) +
+  #     geom_boxplot() +
+  #     geom_jitter(aes(color  = Samples)) +
+  #     theme_bw()
+  # })
 })
